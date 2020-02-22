d6ffde69cb54f5801892938eae249ce2d477a340
Atiqur Rahman <36606917+atique7465@users.noreply.github.com>
2020-02-23 00:13:50 +0600

    Add files via upload

---
+++ rest-servise/src/test/java/com/example/restservise/RestServiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restservise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
